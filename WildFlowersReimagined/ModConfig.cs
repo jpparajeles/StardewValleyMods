77ed65af40bad044fb4f879c43fadc255b94a1ef
José Pablo Parajeles <josepabloparaj@gmail.com>
2024-03-31 12:40:01 -0600

    Add code

---
+++ WildFlowersReimagined/ModConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace WildFlowersReimagined
+{
+    public sealed class ModConfig
+    {
+        public bool ModEnabled { get; set; } = true;
+        public FlowerGrassConfig FlowerGrassConfig { get; set; } = new FlowerGrassConfig();
+        public float WildflowerGrowChance { get; set; } = 0.005f;
+        
+    }
+
+    public sealed class FlowerGrassConfig
+    {
+        public bool UseScythe { get; set; } = true;
+    }
+}
---
+++ WildFlowersReimagined/SaveData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WildFlowersReimagined
+{
+    
+    public sealed class SaveDataItem
+    {
+        public int Vector2X { get; set; }
+        public int Vector2Y { get; set;}
+
+        public int PhaseToShow { get; set; }
+        public int CurrentPhase {  get; set; }
+        public string IndexOfHarvest { get; set; }
+        public byte TintColorR { get; set; }
+        public byte TintColorG { get; set; }
+        public byte TintColorB { get; set; }
+        public byte TintColorA { get; set; }
+        public bool Dead { get; set; }
+
+
+    }
+
+    public sealed class SaveData
+    {
+        public Dictionary<string, List<SaveDataItem>> patchMapData {  get; set; }
+    }
+}
