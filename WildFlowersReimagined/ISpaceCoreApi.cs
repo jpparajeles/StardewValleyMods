8681e511847e638ee90809643709fb556d525fd2
José Pablo Parajeles <josepabloparaj@gmail.com>
2024-04-18 23:36:18 -0600

    Add Spacecore
    Add space core and sync
    
    Current issue: it's not drawing it

---
+++ WildFlowersReimagined/ISpaceCoreApi.cs
@@ -0,0 +1,26 @@
+﻿using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Reflection;
+using Microsoft.Xna.Framework;
+using StardewValley;
+
+namespace WildFlowersReimagined
+{
+    public interface ISpaceCoreApi
+    {
+        string[] GetCustomSkills();
+        int GetLevelForCustomSkill(Farmer farmer, string skill);
+        void AddExperienceForCustomSkill(Farmer farmer, string skill, int amt);
+        int GetProfessionId(string skill, string profession);
+
+        // Must have [XmlType("Mods_SOMETHINGHERE")] attribute (required to start with "Mods_")
+        void RegisterSerializerType(Type type);
+        void RegisterCustomProperty(Type declaringType, string name, Type propType, MethodInfo getter, MethodInfo setter);
+    }
+}
